d9017043ec5e361444aa1af73e923f2e48444844
tuananh <anhtran@foxcommerce.co>
2020-11-26 18:33:29 +0700

    add search similar shops

--- src/pages/seller/similar-shops.js
+++ src/pages/seller/similar-shops.js
@@ -1,9 +1,15 @@
-import SearchBox from "components/SearchBox";
+import { Input } from "antd";
 import Layout from "layout";
 
+const { Search } = Input;
 export default function SimilarShops() {
   return (
     <Layout>
+      <Search placeholder="Shop URL" enterButton="Search" size="large" onSearch={getSimilarShops} />
     </Layout>
   );
+}
+
+const getSimilarShops = (value) => {
+  console.log(value)
 }
--- src/seller/similar-shops/index.js
+++ src/seller/similar-shops/index.js
@@ -1,11 +1,2 @@
-import SearchBox from "components/SearchBox";
 
-export default function SimilarShops() {
-    <div className="gx-main-content">
-        <div className="gx-app-module">
-            <div className="gx-d-block gx-d-lg-none">
-                <SearchBox></SearchBox>
-            </div>
-        </div>
-    </div>
-}
+
