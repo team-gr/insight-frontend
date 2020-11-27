6efb03ee14319cec1f956d88378573a319c6861c
tuananh <anhtran@foxcommerce.co>
2020-11-27 18:17:04 +0700

    setting crawler screen

---
+++ src/pages/admin/crawler.js
@@ -0,0 +1,10 @@
+import CrawlerHome from "admin/crawler/Home";
+import AppLayout from "layout";
+
+export default function Crawler() {
+    return (
+        <AppLayout>
+            <CrawlerHome></CrawlerHome>
+        </AppLayout>
+    )
+}
