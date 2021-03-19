ee92b521855f84cd28432fd724d522d96768e32e
thanhdn <daongocthanh98hy@gmail.com>
2021-03-20 05:50:13 +0700

    update

--- src/app-redux/index.js
+++ src/app-redux/index.js
@@ -3,15 +3,13 @@ import { getThunkExtension } from "redux-dynamic-modules-thunk";
 
 import { getSettingsModule } from "app-redux/settings";
 import { getAuthModule } from "app-redux/auth";
-import { getFeatureCompareModule } from "app-redux/featurecompare";
 
 const store = createStore(
   {
     extensions: [getThunkExtension()],
   },
   getSettingsModule(),
-  getAuthModule(),
-  getFeatureCompareModule()
+  getAuthModule()
 );
 
 export default store;
